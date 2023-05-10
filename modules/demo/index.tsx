31fd5cac9a8c2a1ac039086841b04d80cfcc1124
zoeGuava <zoeguava@gmail.com>
2023-05-10 11:43:03 +0800

    feat: 🎸 init next-app
    next v13.4 but not use app router, still use pages router

---
+++ modules/demo/index.tsx
@@ -0,0 +1,13 @@
+const Demo = () => {
+  return (
+    <div>
+      <h1>Demo</h1>
+      <div className='test'>
+        red text
+        <div className='test2'>blue text</div>
+      </div>
+    </div>
+  );
+}
+
+export default Demo;
